9671938eb7cfebb7c5fe434d79f0c6aa747093e7
unknown <SENA@NEIAPRSINFSD606.FormacionSena.RED>
2024-06-06 14:45:07 -0500

    Cambios en el crear cuenta

--- aplicativoMobil/app/src/main/java/com/example/asignaweb/eliminar_cuenta.kt
+++ aplicativoMobil/app/src/main/java/com/example/asignaweb/eliminar_cuenta.kt
@@ -1,33 +1,20 @@
 package com.example.asignaweb
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class eliminar_cuenta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_eliminar_cuenta)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-    }
-    fun cambioCrearCuenta(view: View){
-        var intent= Intent(application,crear_cuenta::class.java)
-        startActivity(intent)
-    }
-
-    fun modulo_informacion(view: View){
-        var intent= Intent(application,modulo_informacion::class.java)
-        startActivity(intent)
     }
 }
