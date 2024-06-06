1c042b87920f5a50dd0be5421606c291362f1e21
mariana <marianamayaya@gmail.com>
2024-06-06 17:57:36 -0500

    Mi perfil y modulo de informacion

--- aplicativoMobil/app/src/main/java/com/example/asignaweb/modulo_informacion.kt
+++ aplicativoMobil/app/src/main/java/com/example/asignaweb/modulo_informacion.kt
@@ -1,6 +1,8 @@
 package com.example.asignaweb
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,29 @@ class modulo_informacion : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun cambioAuditorio(view: View){
+        var intent= Intent(application,infoAuditorio::class.java)
+        startActivity(intent)
+    }
+
+    fun cambioBiblioteca(view: View){
+        var intent= Intent(application,infoBiblioteca::class.java)
+        startActivity(intent)
+    }
+
+    fun cambioCanchas(view: View){
+        var intent= Intent(application,info_canchas::class.java)
+        startActivity(intent)
+    }
+
+    fun cambioGimnasio(view: View){
+        var intent= Intent(application,info_gimnasio::class.java)
+        startActivity(intent)
+    }
+
+    fun cambiarMiPerfil(view: View){
+        var intent= Intent(application,mi_perfil::class.java)
+        startActivity(intent)
     }
 }
