e4221ef05ab66cbe4481faa7f399cfec9793effd
Mariana Charry <marianamayaya@gmail.com>
2024-07-19 12:19:44 -0500

    Cambios en la aplicacion mobil

--- aplicativoMobil/app/src/main/java/com/example/asignaweb/espacios.kt
+++ aplicativoMobil/app/src/main/java/com/example/asignaweb/espacios.kt
@@ -1,36 +1,20 @@
 package com.example.asignaweb
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class iniciarSesion : AppCompatActivity() {
+class espacios : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_iniciar_sesion)
+        setContentView(R.layout.activity_espacios)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-    fun recuperar_contrasena(view: View){
-        var intent= Intent(application,recuperar_password::class.java)
-        startActivity(intent)
-    }
-
-    fun crear_cuenta(view: View){
-        var intent= Intent(application,crear_cuenta::class.java)
-        startActivity(intent)
-    }
-
-    fun espacios(view: View){
-        var intent= Intent(application,espacios::class.java)
-        startActivity(intent)
     }
 }
